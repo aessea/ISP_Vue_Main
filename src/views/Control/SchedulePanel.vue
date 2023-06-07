56e490dfd7a7b83c41f911dcb88c8afd6672ec0d
FHY <51120966+fuhychn@users.noreply.github.com>
2023-06-07 22:41:55 +0800

    文件名不做严格限制

--- src/views/Control/SchedulePanel.vue
+++ src/views/Control/SchedulePanel.vue
@@ -991,7 +991,7 @@ export default {
     // 主板文件上传钩子
     handleChangeMain(file, fileList) {
       const fileName = file.name.replace(/\.xlsx$/, '')
-      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(主板)(正排|预排)$/
+      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(主板)(正排|预排).*$/
 
       if (!regex.test(fileName)) {
         const tip = '文件命名格式错误，请修改后重新上传！' + `<br/>` + '（正确文件名示例：0901主板预排）'
@@ -1014,7 +1014,7 @@ export default {
     // 小板文件上传钩子
     handleChangeSmall(file, fileList) {
       const fileName = file.name.replace(/\.xlsx$/, '')
-      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(小板)(正排|预排)$/
+      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(小板)(正排|预排).*$/
 
       if (!regex.test(fileName)) {
         const tip = '文件命名格式错误，请修改后重新上传！' + `<br/>` + '（正确文件名示例：0901小板预排）'
