5d33c387d29a7fc626064c4f11f8538f656e9e9c
FuHy_109 <51120966+fuhychn@users.noreply.github.com>
2023-07-19 09:33:58 +0800

    <运行日志>删除一个月前的日志信息

--- src/api/HistoryLog/HistoryRun.js
+++ src/api/HistoryLog/HistoryRun.js
@@ -15,3 +15,11 @@ export function SearchData(data) {
     data
   })
 }
+// 获取表格数据 分页获取
+export function DeleteHistoryLog(data) {
+  return request({
+    url: '/HistoryLog/HistoryRun/delete_history_log/',
+    method: 'post',
+    data
+  })
+}
