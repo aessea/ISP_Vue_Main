129d91b57fc01d9dc79c406ec18b1d2faa1a39ec
FHY <51120966+fuhychn@users.noreply.github.com>
2023-08-02 15:27:14 +0800

    主板制程配置表：新增一列

--- src/views/LongConfig/ProcessData.vue
+++ src/views/LongConfig/ProcessData.vue
@@ -87,6 +87,7 @@
           <el-table-column prop="ct_predict_upper" label="CT预测上限" width="120" />
           <el-table-column prop="process_sequence" label="先后加工顺序" width="120" />
           <el-table-column prop="process_order" label="制程分配点数的优先顺序" width="120" sortable />
+          <el-table-column prop="switch_name" label="原制程名" width="120" sortable />
           <el-table-column width="110" fixed="right" label="操作">
             <template slot-scope="scope">
               <el-button
@@ -194,18 +195,6 @@
             </el-col>
           </el-row>
           <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
-            <el-col :span="12" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.buffer_time" prop="buffer_time" label="上下板间隔时间">
-                <el-input-number v-model="model.buffer_time" placeholder="请输入" :style="{width: '100%'}" />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.is_point" prop="is_point" label="是否按点数">
-                <el-switch v-model="model.is_point" style="width: 100%" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
             <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
               <el-form-item :rules="rules.locked_buffer_time" prop="locked_buffer_time" label="锁定内上下板间隔时间(时)">
                 <el-input-number v-model="model.locked_buffer_time" placeholder="请输入" :style="{width: '100%'}" />
@@ -224,6 +213,23 @@
             <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
               <el-form-item :rules="rules.ct_predict_upper" prop="ct_predict_upper" label="CT预测上限">
                 <el-input-number v-model="model.ct_predict_upper" placeholder="请输入" :style="{width: '100%'}" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
+            <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
+              <el-form-item :rules="rules.buffer_time" prop="buffer_time" label="上下板间隔时间">
+                <el-input-number v-model="model.buffer_time" placeholder="请输入" :style="{width: '100%'}" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
+              <el-form-item :rules="rules.is_point" prop="is_point" label="是否按点数">
+                <el-switch v-model="model.is_point" style="width: 100%" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
+              <el-form-item :rules="rules.switch_name" prop="switch_name" label="原制程名">
+                <el-input v-model="model.switch_name" placeholder="请输入" clearable />
               </el-form-item>
             </el-col>
           </el-row>
@@ -411,7 +417,8 @@ export default {
         ct_predict_upper: 0,
         process_order: '',
         process_sequence: '',
-        onehot_code: ''
+        onehot_code: '',
+        switch_name: ''
       },
       // 修改前的表单内容，用于对比表单前后的变化（应用：关闭前提示修改未保存）
       modelOriginal: {
@@ -433,7 +440,8 @@ export default {
         ct_predict_upper: 0,
         process_order: '',
         process_sequence: '',
-        onehot_code: ''
+        onehot_code: '',
+        switch_name: ''
       },
       rules: {
         name: [{
@@ -514,6 +522,11 @@ export default {
           trigger: 'blur'
         }],
         process_order: [{
+          required: true,
+          message: '不能为空',
+          trigger: 'blur'
+        }],
+        switch_name: [{
           required: true,
           message: '不能为空',
           trigger: 'blur'
