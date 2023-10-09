5e0efeef115825039f3e8c27d4bf25b88482c6e4
FHY <51120966+fuhychn@users.noreply.github.com>
2023-10-09 17:00:37 +0800

    登录页面：去掉无用代码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,24 +8,10 @@
       <div class="form-box">
         <div class="login-container">
           <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
-
             <div class="title-container">
               <h3 class="title">SMT排程系统</h3>
             </div>
-
             <el-form-item prop="username">
-              <!-- <span class="svg-container">
-                <svg-icon icon-class="user" />
-              </span>
-              <el-input
-                ref="username"
-                v-model="loginForm.username"
-                placeholder="请输入用户名"
-                name="username"
-                type="text"
-                tabindex="1"
-                autocomplete="on"
-              /> -->
               <el-input
                 ref="username"
                 v-model="loginForm.username"
@@ -48,30 +34,6 @@
                 size="big"
               />
             </el-form-item>
-            <!-- <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-              <el-form-item prop="password">
-                <span class="svg-container">
-                  <svg-icon icon-class="password" />
-                </span>
-                <el-input
-                  :key="passwordType"
-                  ref="password"
-                  v-model="loginForm.password"
-                  :type="passwordType"
-                  placeholder="请输入密码"
-                  name="password"
-                  tabindex="2"
-                  autocomplete="on"
-                  @keyup.native="checkCapslock"
-                  @blur="capsTooltip = false"
-                  @keyup.enter.native="handleLogin"
-                />
-                <span class="show-pwd" @click="showPwd">
-                  <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-                </span>
-              </el-form-item>
-            </el-tooltip> -->
-
             <el-button
               :loading="loading"
               type="primary"
@@ -84,14 +46,6 @@
               登录
             </el-button>
           </el-form>
-
-          <!-- <el-dialog title="Or connect with" :visible.sync="showDialog">
-            Can not be simulated on local, so please combine you own business simulation! ! !
-            <br>
-            <br>
-            <br>
-            <social-sign />
-          </el-dialog> -->
         </div>
       </div>
     </el-card>
