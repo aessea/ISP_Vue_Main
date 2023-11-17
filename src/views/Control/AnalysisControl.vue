da5c95580f7b411b18dfaed5e7022cf53d84a345
FHY <51120966+fuhychn@users.noreply.github.com>
2023-11-17 23:01:48 +0800

    上传的文件不限制ABL

--- src/views/Control/AnalysisControl.vue
+++ src/views/Control/AnalysisControl.vue
@@ -558,7 +558,7 @@ export default {
     // 上传文件
     loadExcelFile(file, fileList) {
       const fileName = file.name.replace(/\.xlsx$/, '')
-      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(主板|小板)(正排|预排).*$/
+      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(主板|小板|ABL)(正排|预排).*$/
 
       if (!regex.test(fileName)) {
         const tip = '文件命名格式错误，请修改后重新上传！' + `<br/>` + '（正确文件名示例：0901主板预排）'
