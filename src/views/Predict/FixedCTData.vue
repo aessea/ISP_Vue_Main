ed5dfb40e8901454c6733feda6d924b62360d960
FHY <51120966+fuhychn@users.noreply.github.com>
2024-01-02 17:39:38 +0800

    车间指定CT表：去重

--- src/views/Predict/FixedCTData.vue
+++ src/views/Predict/FixedCTData.vue
@@ -110,36 +110,34 @@
       :before-close="handleFormClose"
       @dragDialog="handleDrag"
     >
-      <el-card class="card-form" shadow="never">
-        <el-form ref="$form" :model="model" label-position="left" size="small">
-          <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
-            <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.line" prop="line" label="线别">
-                <el-select v-model="model.line" placeholder="请选择线别" :style="{width: '100%'}">
-                  <el-option v-for="(item) in lineOptions" :key="item.value" :label="item.label" :value="item.value" :disabled="!!item.disabled" />
-                </el-select>
-              </el-form-item>
-            </el-col>
-            <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.SMT_machine_name" prop="SMT_machine_name" label="SMT机种名">
-                <el-input v-model="model.SMT_machine_name" placeholder="请输入" clearable />
-              </el-form-item>
-            </el-col>
-            <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.process" prop="process" label="制程">
-                <el-select v-model="model.process" placeholder="请选择制程" :style="{width: '100%'}">
-                  <el-option v-for="(item) in processOptions" :key="item.value" :label="item.label" :value="item.value" :disabled="!!item.disabled" />
-                </el-select>
-              </el-form-item>
-            </el-col>
-            <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
-              <el-form-item :rules="rules.CT" prop="CT" label="指定CT">
-                <el-input-number v-model="model.CT" placeholder="请输入" :style="{width: '100%'}" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-form>
-      </el-card>
+      <el-form ref="$form" :model="model" label-position="left" size="small">
+        <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
+          <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
+            <el-form-item :rules="rules.line" prop="line" label="线别">
+              <el-select v-model="model.line" placeholder="请选择线别" :style="{width: '100%'}">
+                <el-option v-for="(item) in lineOptions" :key="item.value" :label="item.label" :value="item.value" :disabled="!!item.disabled" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
+            <el-form-item :rules="rules.SMT_machine_name" prop="SMT_machine_name" label="SMT机种名">
+              <el-input v-model="model.SMT_machine_name" placeholder="请输入" clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
+            <el-form-item :rules="rules.process" prop="process" label="制程">
+              <el-select v-model="model.process" placeholder="请选择制程" :style="{width: '100%'}">
+                <el-option v-for="(item) in processOptions" :key="item.value" :label="item.label" :value="item.value" :disabled="!!item.disabled" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6" :offset="0" :push="0" :pull="0" tag="div">
+            <el-form-item :rules="rules.CT" prop="CT" label="指定CT">
+              <el-input-number v-model="model.CT" placeholder="请输入" :style="{width: '100%'}" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="handleFormClose">关闭</el-button>
         <el-button v-if="dialogBtnType === true" type="primary" @click="addData">添加</el-button>
@@ -402,26 +400,18 @@ export default {
       data['user_name'] = this.name
       this.$refs['$form'].validate(async(valid) => {
         if (valid) {
-          // 若不存在同组件同线体的记录，允许添加
-          if (!await this.hasSameRow(data)) {
-            AddData(data).then(res => {
-              if (res.code === 20000) {
-                this.$notify({
-                  title: '添加成功',
-                  message: '成功添加 1 条数据',
-                  type: 'success'
-                })
-                this.model = deepClone(this.modelBackup)
-                this.modelOriginal = deepClone(this.modelBackup)
-                this.refreshTableData(true)
-              }
-            })
-          } else {
-            this.$message({
-              type: 'error',
-              message: '已存在同组件同线别的记录，无需重复添加！'
-            })
-          }
+          AddData(data).then(res => {
+            if (res.code === 20000) {
+              this.$notify({
+                title: '添加成功',
+                message: '成功添加 1 条数据',
+                type: 'success'
+              })
+              this.model = deepClone(this.modelBackup)
+              this.modelOriginal = deepClone(this.modelBackup)
+              this.refreshTableData(true)
+            }
+          })
         } else {
           this.$message({
             type: 'error',
