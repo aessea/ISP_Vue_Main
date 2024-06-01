9ea9a7dec222676c68e839789edc1a77e29559b0
FHY <51120966+fuhychn@users.noreply.github.com>
2024-06-01 20:34:22 +0800

    国际化：每日配置新增2张表

--- src/views/DayConfig/PackagingLineHoliday.vue
+++ src/views/DayConfig/PackagingLineHoliday.vue
@@ -51,8 +51,8 @@
           @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55" />
-          <el-table-column prop="packaging_line_name" label="包装线名" sortable />
-          <el-table-column prop="holiday_days" label="放假天数" />
+          <el-table-column prop="packaging_line_name" :label="lang_dict.packaging_line_name" sortable />
+          <el-table-column prop="holiday_days" :label="lang_dict.holiday_days" />
           <el-table-column width="110" fixed="right" :label="$t('TablePage.TitleOperate')">
             <template slot-scope="scope">
               <el-button
@@ -97,12 +97,12 @@
       <el-form ref="$form" :model="model" label-position="left" size="small">
         <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
           <el-col :span="12" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.packaging_line_name" prop="packaging_line_name" label="包装线名">
+            <el-form-item :rules="rules.packaging_line_name" prop="packaging_line_name" :label="lang_dict.packaging_line_name">
               <el-input v-model="model.packaging_line_name" :placeholder="$t('Placeholder.Enter')" clearable />
             </el-form-item>
           </el-col>
           <el-col :span="12" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_days" prop="holiday_days" label="放假天数">
+            <el-form-item :rules="rules.holiday_days" prop="holiday_days" :label="lang_dict.holiday_days">
               <el-input-number v-model="model.holiday_days" :placeholder="$t('Placeholder.Enter')" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
--- src/views/DayConfig/ProcessAiHoliday.vue
+++ src/views/DayConfig/ProcessAiHoliday.vue
@@ -51,9 +51,9 @@
           @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55" />
-          <el-table-column prop="post_process_name" label="后工序" sortable />
-          <el-table-column prop="holiday_begin" label="放假开始时间" sortable />
-          <el-table-column prop="holiday_end" label="放假结束时间" sortable />
+          <el-table-column prop="post_process_name" :label="lang_dict.post_process_name" sortable />
+          <el-table-column prop="holiday_begin" :label="lang_dict.holiday_begin" sortable />
+          <el-table-column prop="holiday_end" :label="lang_dict.holiday_end" sortable />
           <el-table-column width="110" fixed="right" :label="$t('TablePage.TitleOperate')">
             <template slot-scope="scope">
               <el-button
@@ -98,18 +98,18 @@
       <el-form ref="$form" :model="model" label-position="left" size="small">
         <el-row :gutter="20" type="flex" justify="start" align="top" tag="div">
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.post_process_name" prop="post_process_name" label="后工序">
+            <el-form-item :rules="rules.post_process_name" prop="post_process_name" :label="lang_dict.post_process_name">
               <el-input v-model="model.post_process_name" :placeholder="$t('Placeholder.Enter')" clearable />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_begin" prop="holiday_begin" label="放假开始时间">
-              <el-date-picker v-model="model.holiday_begin" type="datetime" placeholder="请选择日期时间" value-format="yyyy-MM-dd HH:mm:ss" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.holiday_begin" prop="holiday_begin" :label="lang_dict.holiday_begin">
+              <el-date-picker v-model="model.holiday_begin" type="datetime" :placeholder="$t('Placeholder.Select')" value-format="yyyy-MM-dd HH:mm:ss" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
           <el-col :span="8" :offset="0" :push="0" :pull="0" tag="div">
-            <el-form-item :rules="rules.holiday_end" prop="holiday_end" label="放假结束时间">
-              <el-date-picker v-model="model.holiday_end" type="datetime" placeholder="请选择日期时间" value-format="yyyy-MM-dd HH:mm:ss" :style="{width: '100%'}" />
+            <el-form-item :rules="rules.holiday_end" prop="holiday_end" :label="lang_dict.holiday_end">
+              <el-date-picker v-model="model.holiday_end" type="datetime" :placeholder="$t('Placeholder.Select')" value-format="yyyy-MM-dd HH:mm:ss" :style="{width: '100%'}" />
             </el-form-item>
           </el-col>
         </el-row>
